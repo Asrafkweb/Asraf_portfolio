ed387a2947e17d165283f5518f1b4037f11d5180
Asrafkweb <asrafalikhan757@gmail.com>
2024-06-15 21:30:20 +0300

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -17,12 +17,22 @@ var projects =[{
 },
 {
   
-  image:'images /myntra.jpg',
+  image:'myntra/images/myntra.jpg',
   project_name:'Myntra ecommerce clone',
   project_discription:'this is myntra ecommerce website clone fully functional with Cart options',
   using:['HTML','CSS','JavaScript','JQuery','Bootstrap'],
   link:'/myntra/index.html'
 
 
-}
+},
+  {
+  
+  image:'Eid/images/eid.jpg',
+  project_name:'Eid Mubark !',
+  project_discription:'This project take name from user and create dynamic a wishes for user and there is too many options',
+  using:['HTML','CSS','JavaScript'],
+  link:'Eid/index.html'
+
+
+    }           
 ]
