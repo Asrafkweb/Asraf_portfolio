3e577c9b101a882957ecc279dd895f98848f7e99
Asrafkweb <asrafalikhan757@gmail.com>
2024-06-15 03:23:16 +0300

    Add files via upload

---
+++ js/projects.js
@@ -0,0 +1,28 @@
+var projects =[{
+  image:'images /project-1.jpg',
+  project_name:'Videos player & Contacts',
+  project_discription:'Tihis is a videos player & Contact Saver. with signup & logIn function',
+  using:['HTML','Css','JavaScript'],
+  link:'/my portfolio website /myntra/index.html'
+   
+  
+},
+{
+  
+  image:'images /skill.png',
+  project_name:'app',
+  project_discription:'i am description',
+  using:['HTML','CSS','JavaScript']
+
+},
+{
+  
+  image:'images /myntra.jpg',
+  project_name:'Myntra ecommerce clone',
+  project_discription:'this is myntra ecommerce website clone fully functional with Cart options',
+  using:['HTML','CSS','JavaScript','JQuery','Bootstrap'],
+  link:'/myntra/index.html'
+
+
+}
+]
---
+++ js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+  $(".p_bar").animate({width:'80%'},1000,function () {
+    $('.p_bar-css').animate({width:'80%'},1000,function () {
+      $(".p_bar-c3").animate({width:'60%'},1000,function(){
+        $('.p_bar-js').animate({width:'75%'},1000,function(){
+          $('.p_bar-jquery').animate({width:'85%'},1000);
+        });
+      });
+    });
+  });
+});
+var projects_parent=document.querySelector('#parent');
+
+projects.forEach((data)=>{
+  projects_parent.innerHTML+=`
+              <div class="col-sm-4 ">
+                  <div class="card project-content">
+                  <div class=" project-img">
+                      <img src="${data.image}" class="card-img-top" alt="Project 1">
+                      </div>
+                    <div class="card-body">
+                    <h5 class="card-title">${data.project_name}</h5>
+                    <p class="card-text">${data.project_discription}</p>
+              <div class="using">
+
+             </div>
+                    <a href="${data.link}" class="btn btn-primary">View Project</a>
+                      </div>
+                  </div>
+              </div>
+             
+  `
+  
+})
+projects.forEach((data, index) => {
+  // Select the specific .using div for the current project
+  var parent = projects_parent.querySelectorAll('.using')[index];
+  lang(data.using, parent);
+});
+
+
+
+function lang(all_lang,parent) {
+
+all_lang.forEach((lang)=>{
+  var span =document.createElement('span');
+  span.innerHTML=lang;
+  parent.appendChild(span);
+})
+
+}
+
+
+
